80265999cf2924a8bfa3745808c7bce0eff73761
Emmanuel Chêne <emmanuel.chene@etu.unice.fr>
2017-02-20 16:05:16 +0100

    Correction d'un bug

--- TD2/app/src/main/java/com/example/emmanuel/td/NumberPicker.java
+++ TD2/app/src/main/java/com/example/emmanuel/td/NumberPicker.java
@@ -8,20 +8,17 @@ import android.view.View;
 
 public class NumberPicker extends Activity{
 
-    NumberPicker MyNumPicker1;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         //setContentView(R.layout.activity_main);
         setContentView(R.layout.numberpick_layout);
 
-        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View v1 = inflater.inflate(R.layout.numberpick_layout, null);
-/*        MyNumPicker1 = (NumberPicker) v1.findViewById(R.id.MyNunPicker1);
-
-        MyNumPicker1.setMinValue(10);
-        MyNumPicker1.setMaxValue(20);*/
+        final android.widget.NumberPicker numberPicker = (android.widget.NumberPicker)findViewById(R.id.NumberPick);
+        numberPicker.setMinValue(10);
+        numberPicker.setMaxValue(20);
+        numberPicker.setWrapSelectorWheel(false);
 
     }
 
